1bef36abd1af05d7677c287b97d28ca7b92941db
ohidursohag <ohidursohag9595@gmail.com>
2023-11-06 04:57:29 +0600

    Update conditionally render login and logout button and user Profile if user loggedin or not

--- src/Components/NavbarComponents/LoginAndProfile.jsx
+++ src/Components/NavbarComponents/LoginAndProfile.jsx
@@ -1,18 +1,59 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Profile from "./Profile";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const LoginAndProfile = () => {
-   return(
+   const { user, logOut } = useAuth()
+   const location = useLocation()
+   // console.log(user);
+
+   const handleLogOut = () => {
+      Swal.fire({
+         title: 'Are you sure?',
+         text: "You want to sign out?",
+         icon: 'warning',
+         showCancelButton: true,
+         confirmButtonColor: '#3085d6',
+         cancelButtonColor: '#d33',
+         confirmButtonText: 'Confirm'
+      }).then(async (result) => {
+         if (result.isConfirmed) {
+            logOut()
+               .then(() => {
+                  Swal.fire({
+                     icon: 'success',
+                     title: 'Sucessfully Signed Out',
+                  })
+               })
+               .catch(err => { console.error(err.message) })
+         }
+
+      })
+
+   }
+
+   return (
       <>
-         <div className="">
-            <NavLink to="/login" className={({ isActive }) => isActive ? 'btn  bg-[#DCB342]/70 text-lg hover:bg-[#DCB342] text-black border-none btn-sm  normal-case w-full rounded-[4px]' : 'w-full btn btn-ghost text-lg btn-sm normal-case rounded-[4px]'}>
-               Login
-            </NavLink>
-         </div>
-         <div className="flex justify-center items-center mt-5 md:mt-0">
-            <Profile></Profile>
-         </div>
+         {
+            user
+               ? <>
+                  <div className="">
+                     <button onClick={handleLogOut}  className= 'btn  bg-[#DCB342]/70 text-lg hover:bg-[#DCB342] text-black border-none btn-sm  normal-case w-full rounded-[4px]'>
+                        Log Out
+                     </button>
+                  </div>
+                  <div className="flex justify-center items-center mt-5 md:mt-0">
+                     <Profile></Profile>
+                  </div>
+               </>
+               : <NavLink to={location.pathname === '/login' ? '/register' : location.pathname === '/register' ? '/login' : '/login'}>
+                  <span className={` btn btn-ghost rounded-[4px] btn-sm normal-case text-lg ${location.pathname === '/login' || location.pathname === '/register' ? 'btn  bg-[#DCB342]/70 hover:bg-[#DCB342] rounded-[4px] text-black border-none btn-sm  normal-case' : ''} `} >{location.pathname === '/login' ? 'Register' : location.pathname === '/register' ? 'Login' : 'Login'}</span>
+               </NavLink>
+         }
+
       </>
-   )}
+   )
+}
 export default LoginAndProfile;
--- src/Components/NavbarComponents/Profile.jsx
+++ src/Components/NavbarComponents/Profile.jsx
@@ -1,11 +1,14 @@
 
 import { NavLink } from 'react-router-dom';
 import avaterPlaceHolder from '../../assets/Icon/avater.png'
+import auth from '../../Config/Firebase.config';
 const Profile = () => {
+   const user = auth.currentUser
+   console.log(user);
    return (
       <div id='profileDropdown' tabIndex={0} className="dropdown flex justify-center items-center">         
-            <button className="btn btn-circle md:btn-md p-0 mx-auto">
-               <img src={avaterPlaceHolder} />
+         <button className="btn btn-circle border-[3px] border-[#282531] focus:border-[#DCB342] hover:border-[#DCB342] md:btn-md p-0 mx-auto">
+            <img className='rounded-full' src={user?.photoURL ||avaterPlaceHolder} />
             </button>
          <div tabIndex={0} className="dropdown-content  z-[1] menu pt-5 md:shadow md:bg-base-100 rounded-lg w-max md:right-0 top-[50px] space-y-4">
  
